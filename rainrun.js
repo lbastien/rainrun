2065f406831b08d7c94b6cdf6d1f6dd0637387aa
lbastien <lucpahia@lucbastien.com>
2016-12-20 16:57:06 -0500

    add new stuff to start screen

--- rainrun.js
+++ rainrun.js
@@ -1,13 +1,24 @@
+
 var xPositions = [];
 var yPositions = [];
 var colorArray = [];
 
 draw = function() //set up the starting page
 {
+    background(0);
 fill(255);
-fill(0); 
 textSize(70);
 text("RAIN RUN", 38, 133);
+textSize(20);
+text("Click anywhere to continue", 100, 175);
+textSize(15);
+text("You could click here.", 50, 200);
+text("Or here.", 200, 225);
+text("Or here.", 175, 275);
+text("Or here.", 300, 300);
+text("I wouldn't care." , 100, 325);
+text("Just click SOMEWHERE, 'cause I'm getting impatient.", 25, 350);
+text("Still here? Well, MOVE IT!", 150, 375);
 mouseClicked = function() //if the mouse is clicked...
     {
         draw = function() //...then start the game
@@ -33,16 +44,3 @@ mouseClicked = function() //if the mouse is clicked...
         }; //close of inner draw function
     }; //close of mouse clicked function
 }; //close of outer draw function
-
-
-
-
-
-
-
-
-
-
-
-
-
